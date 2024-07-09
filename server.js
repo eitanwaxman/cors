aba39d1ce5677680550c917461d55571199728a7
Eitan <eitanwaxman@gmail.com>
2024-07-09 12:37:38 -0400

    add cookie parser

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+    console.log('Cookies:', req.cookies);
+    next();
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
