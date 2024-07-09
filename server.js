3282f090f3c7bb51264c8d905243d2ff1f0bda93
Eitan <eitanwaxman@gmail.com>
2024-07-09 13:35:47 -0400

    allow wikipedia

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Origin', 'https://wikipedia.org'); 
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'); 
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
