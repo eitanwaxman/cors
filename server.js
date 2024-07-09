d90b270b960a9fb41590a2eee00561be4546cd39
Eitan <eitanwaxman@gmail.com>
2024-07-09 14:27:42 -0400

    remove headers

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://www.wikipedia.org'); 
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'); 
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-});
-
 app.use(cookieParser());
 
 app.use((req, res, next) => {
